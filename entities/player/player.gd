1e9ccf48433a1ebaeba0792f90de21159062295b
LEGENDARY-KING <aarushsalgania06@gmail.com>
2026-06-09 23:10:44 +0530

    Setup base player.gd skeleton with dynamic gravity

---
+++ entities/player/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+class_name Player
+
+# Core Variables
+
+@export var speed: float = 300.0
+var facing_direction: int = 1
+
+# References
+@onready var sprite = $Sprite2D
+@onready var fsm = $fsm
+
+# Jump/Gravity Variables
+
+@export var jump_velocity: float = -500.0
+@export var jump_gravity_multiplier: float = 0.8 # Floaty Jump
+@export var fall_gravity_multiplier: float = 2.0 # Heavy Fall
+@export var max_jumps: int = 2
+var current_jumps: int = 0
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float):
+	
+	var direction = Input.get_axis("move_left","move_right")
+	if direction != 0:
+		facing_direction = sign(direction)
+		sprite.flip_h = (direction < 0)
+		
+	if not is_on_floor():
+		if velocity.y < 0 and Input.is_action_pressed("jump"):
+			velocity += get_gravity() * jump_gravity_multiplier * delta
+		else:
+			velocity += get_gravity() * fall_gravity_multiplier * delta
+	else:
+		current_jumps = max_jumps
+		
+	move_and_slide()
