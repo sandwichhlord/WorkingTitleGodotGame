7277658f5e5b303154a118944d3c70282c4e3327
icecrow-ctrl <guryansh26@gmail.com>
2026-06-11 12:21:24 +0530

    updated miniboss with positions and light switch

--- entities/minibosses/miniboss.gd
+++ entities/minibosses/miniboss.gd
@@ -4,6 +4,15 @@ class_name MiniBoss
 @export var current_stage:int = 1
 @export var is_invulnerable:bool = false
 
+# locations on this board for boss
+@export var top : Vector2 = Vector2(640,150)
+@export var left : Vector2 = Vector2(340,450)
+@export var right : Vector2 = Vector2(940,450)
+@export var speed : float = 200.0
+
+@export var target: Vector2 = Vector2.ZERO
+@export var is_light_on : bool = true
+
 @onready var health_component = $HealthComponent
 @onready var fsm_node = $fsm
 
@@ -12,6 +21,8 @@ func _ready() -> void:
 		health_component.health_changed.connect(_on_health_changed)  #changes states
 		health_component.health_changed.connect(_on_boss_died)       
 		
+	global_position = top
+	target = top
 
 func _on_health_changed(current_health, max_health) -> void:
 	if is_invulnerable:
@@ -24,5 +35,14 @@ func _on_health_changed(current_health, max_health) -> void:
 		fsm_node.change_state("Handler")
 		
 
-func _on_boss_died() -> void:
-	queue_free()
+func _on_boss_died(current_health, max_health) -> void:
+	if current_health <= 0:
+		queue_free()
+
+
+func lighting(light_is_on : bool) -> void:
+	is_light_on = light_is_on
+	
+	if not is_light_on:
+		target = top
+		fsm_node.change_state("move")
