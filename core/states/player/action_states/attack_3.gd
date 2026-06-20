bc5c83a2928aba8a59712fbbb3b77218681b555a
LEGENDARY-KING <aarushsalgania06@gmail.com>
2026-06-20 22:09:30 +0530

    Created the third attack state

--- core/states/player/action_states/attack_3.gd
+++ core/states/player/action_states/attack_3.gd
@@ -1,11 +1,26 @@
 extends State
 
+@export var attack_damage: float = 30.0
+@export var attack_duration: float = 0.6
+@export var target_layer: int = 2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var anim_player = owner.get_node("AnimationPlayer")
 
+func enter():
+	entity.is_movement_locked = true
+	anim_player.play("swing_3")
+	
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2(101, 60)
+	var hitbox = Hitbox.new(entity.get_total_damage(attack_damage), attack_duration, shape, target_layer)
+	entity.add_child(hitbox)
+	hitbox.position.x = 50 * entity.facing_direction
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func exit():
+	entity.is_movement_locked = false
+
+func update(_delta: float):
 	pass
+
+func _on_animation_finished():
+	fsm.change_state("Ready")
