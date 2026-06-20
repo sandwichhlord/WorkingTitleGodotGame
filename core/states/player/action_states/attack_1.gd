af900ee64f3b022dceead1e9a5c4effd15cc2f75
LEGENDARY-KING <aarushsalgania06@gmail.com>
2026-06-20 22:08:57 +0530

    Created the first attack state and ability to go into next combo

--- core/states/player/action_states/attack_1.gd
+++ core/states/player/action_states/attack_1.gd
@@ -1,11 +1,30 @@
 extends State
 
+@export var attack_damage: float = 10.0
+@export var attack_duration: float = 0.3
+@export var target_layer: int = 2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var queued_next_attack: bool = false
+@onready var anim_player = owner.get_node("AnimationPlayer")
 
+func enter():
+	queued_next_attack = false
+	anim_player.play("swing_1")
+	
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2(40, 40)
+	var hitbox = Hitbox.new(entity.get_total_damage(attack_damage), attack_duration, shape, target_layer)
+	
+	entity.add_child(hitbox)
+	hitbox.position.x = 30 * entity.facing_direction
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func update(_delta: float):
+	if Input.is_action_just_pressed("attack"):
+		queued_next_attack = true
+		
+
+func _on_animation_finished():
+	if queued_next_attack:
+		fsm.change_state("Attack_2")
+	else:
+		fsm.change_state("Ready")
