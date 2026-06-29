81d65b7339eba07c767f9ac713c10b779eef3587
Auldron <aarushsalgania06@gmail.com>
2026-06-29 12:17:45 +0530

    Added ranged_attack button press to change state check

--- core/states/player/action_states/ready.gd
+++ core/states/player/action_states/ready.gd
@@ -3,3 +3,5 @@ extends State
 func update(_delta: float):
 	if Input.is_action_just_pressed("attack"):
 		fsm.change_state("Attack_1")
+	elif Input.is_action_just_pressed("shoot"):
+		fsm.change_state("Ranged_Attack")
