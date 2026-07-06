9fe6317d23f53e0cab90714ec7af516c4141d037
warmthunder <hriday.singh99@gmail.com>
2026-07-06 22:08:24 +0530

    added basic particles for jump, walk, explosion

--- core/components/HitboxComponent.gd
+++ core/components/HitboxComponent.gd
@@ -1,22 +1,25 @@
 extends Area2D
 class_name  Hitbox
 
-var hitbox_lifetime: float
-var shape : Shape2D
-var target_mask: int
-var damage: float
-
+@export var hitbox_lifetime: float
+@export var shape: Shape2D
+@export var target_mask: int
+@export var damage: float
 # just assigning the variables on initialization
 # as this isnt a permanent thing and only appears when attacks
 #
 # For infinitely long hitbox, set timer = 0.0
 func _init(_damage: float, _hitbox_lifetime : float, _shape : Shape2D, _target_mask: int) -> void:
+	
 	hitbox_lifetime = _hitbox_lifetime
 	shape = _shape
 	target_mask = _target_mask
 	damage = _damage
 	
+
+
 func _ready() -> void:
+	#print("ready is run")
 	monitorable = false
 	area_entered.connect(_on_area_entered)
 	
@@ -30,16 +33,22 @@ func _ready() -> void:
 		var collision_shape = CollisionShape2D.new()
 		collision_shape.shape = shape
 		add_child(collision_shape)
+	
+	#print("Shape:", shape)
+	#print("Children:", get_children())
 		
 	# refer to hurtbox script, same thing happening here
 	set_collision_layer_value(1,false)
 	set_collision_mask_value(1, false)
 	set_collision_mask_value(target_mask, true)
 	
+
 # default on area entered script
 # if area doesnt have recieve hit dont do anything
+
 func _on_area_entered(area: Area2D) -> void:
 	#print("area entered")
+	#print(area.name)
 	if not area.has_method("recieve_hit"):
 		return
 	area.recieve_hit(damage)
@@ -47,3 +56,5 @@ func _on_area_entered(area: Area2D) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
